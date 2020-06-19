e2c4eeeaf29bd17115a45738426739bd122e4b43
Tore Friis <torefriis@gmail.com>
2020-06-19 21:03:32 +0200

    First version! - not very playable at all

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+import time
+import math
+
+pygame.init()
+width, height = 900, 1600
+screen = pygame.display.set_mode((width, height))
+
+# MAIN CHARACTER
+
+
+class HeroCharacter:
+    def __init__(self, img='ball.png', x=None, y=None):
+        self.IMG = pygame.image.load(img)
+        self.velocity_y = 0
+        self.velocity_x = 0
+        self.acceleration_y = -10
+        self.in_motion = False
+        self.width = self.IMG.get_rect().width
+        self.height = self.IMG.get_rect().height
+        self.x = width - 100 - self.width if x is None else x
+        self.y = height - 300 if y is None else y
+        self.pointer_angle = 0
+
+    def show(self):
+        screen.blit(self.IMG, (self.x, self.y))
+
+    def jump(self, dx, dy):
+        self.velocity_y -= dy/3
+        self.velocity_x -= dx/3
+
+    def update_variables(self):
+        self.velocity_y += self.acceleration_y
+        self.y -= self.velocity_y
+        self.x -= self.velocity_x
+
+    def show_pointer(self, end_pos):
+        pygame.draw.line(screen, (255, 255, 255), (self.x, self.y), end_pos)
+
+
+# INITIATE MAIN CHARACTER AS MAN
+man = HeroCharacter()
+
+running = True
+while running:
+    time.sleep(.05)
+    # EVENTS
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
+    mouse_pos = pygame.mouse.get_pos()
+    if pygame.mouse.get_pressed()[0] == 1 and not man.in_motion:
+        man.in_motion = True
+        man.jump(mouse_pos[0] - man.x, mouse_pos[1] - man.y)
+    # GENERAL SCREEN
+    screen.fill((0, 10, 10))
+    pygame.draw.rect(screen, (0, 0, 0), (width-100, 0, 100, height))
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 100, height))
+    # HP UPDATE
+    if man.y > height:
+        man.__init__()
+    if man.in_motion:
+        man.update_variables()
+    else:
+        man.show_pointer(mouse_pos)
+    man.show()
+
+    pygame.display.update()
+
