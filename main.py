0bbc7bf5ef27cccab673180943f21287b1612bd6
Tore Friis <torefriis@gmail.com>
2020-06-21 16:57:47 +0200

    obstacles working better

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ class HeroCharacter:
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def jump(self, dx, dy):
-        print(dx, dy)
         self.velocity_y += dy / 4
         self.velocity_x += dx / 4
         self.in_motion = True
@@ -43,6 +42,8 @@ class HeroCharacter:
         self.y += self.velocity_y
         self.x += self.velocity_x
         self.score -= int(self.velocity_y/(self.width/2))
+        if self.high_score < self.score:
+            self.high_score = self.score
 
     def show_pointer(self, end_pos):
         pygame.draw.line(screen, (255, 255, 255), (self.x + self.width/2, self.y + self.height/2), end_pos)
@@ -77,7 +78,7 @@ class Obstacle:
     def __init__(self):
         self.side = random.randint(1, 2)
         self.width = random.randint(100, 200)
-        self.height = random.randint(50, 200)
+        self.height = random.randint(200, 500)
         self.y = -self.height
         self.x = width - 100 - self.width if self.side == 1 else 100
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
@@ -140,9 +141,6 @@ while running:
     screen.fill((0, 10, 10))
     pygame.draw.rect(screen, (0, 0, 0), (width - 100, 0, 100, height))
     pygame.draw.rect(screen, (0, 0, 0), (0, 0, 100, height))
-    # "CAMERA" MOVEMENT
-    camera_off = man.starting_y - man.y
-    global_downwards = camera_off if camera_off > 0 else 0
 
     # OBSTACLES UPDATE
     for i in ob_list:
@@ -151,13 +149,16 @@ while running:
         i.move()
         if i.y > height:
             ob_list.remove(i)
-        elif i.y > 100 and i.deploy_ready:
+        elif i.y > 500 and i.deploy_ready:
             ob_list.append(Obstacle())
             i.deploy_ready = False
         # COLLISION
         if man.hitbox.colliderect(i.hitbox):
             running = False
         i.show()
+    # "CAMERA" MOVEMENT
+    camera_off = man.starting_y - man.y
+    global_downwards = camera_off if camera_off > 0 else 0
 
     # MAIN CHARACTER UPDATE
     if not(height - man.height > man.y > 0):
@@ -173,10 +174,8 @@ while running:
     # WALL DETECTION
     if man.x < 100:
         man.hit_wall(1)
-        print('wall hit')
     elif man.x > width - 100 - man.width:
         man.hit_wall(0)
-        print('wall hit')
     # SLOWLY FALLING
     man.y += global_downwards
     man.show()
